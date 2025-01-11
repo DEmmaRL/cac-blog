5dd0d16780835a4045d5fb5d47973020cfbe0d6d
DEmmaRL <demmarlmiau@gmail.com>
2025-01-10 23:28:32 -0600

    add Tailwind CSS support and new landing page SVGs

--- src/components/HomepageFeatures/index.tsx
+++ src/components/HomepageFeatures/index.tsx
@@ -5,54 +5,71 @@ import styles from './styles.module.css';
 import Link from '@docusaurus/Link';
 import 'bootstrap-icons/font/bootstrap-icons.css';
 
-
 type FeatureItem = {
   title: string;
   Svg: React.ComponentType<React.ComponentProps<'svg'>>;
+  backgroundSvg: React.ComponentType<React.ComponentProps<'svg'>>;
   description: ReactNode;
 };
 
 const FeatureList: FeatureItem[] = [
   {
     title: '¡Únete al Discord oficial!',
-    Svg: require('@site/static/img/undraw_docusaurus_mountain.svg').default,
+    Svg: require('@site/static/img/landing-page/discover.svg').default,
+    backgroundSvg: require('@site/static/img/landing-page/blob-1.svg').default,
     description: (
       <>
         <div>
         Nuestro centro de operaciones, contamos con canales donde discutimos problemas, hacemos CV review, recomendamos y solucionamos dudas, mantente al tanto de nuevos concursos y dinámicas, también hay un canal de memes!
         </div>
         <Link
-        className="button button--primary"
+        className="button button--primary mt-3"
         to="blog/TODO">
         Haz click aquí 
-        <i className="bi bi-discord p-3"></i>
+        <i className="bi bi-discord ml-3"></i>
         </Link>  
       </>
     ),
   },
   {
-    title: 'Focus on What Matters',
-    Svg: require('@site/static/img/undraw_docusaurus_tree.svg').default,
+    title: 'Consulta la documentación',
+    Svg: require('@site/static/img/landing-page/operation.svg').default,
+    backgroundSvg: require('@site/static/img/landing-page/blob-2.svg').default,
     description: (
       <>
-        Docusaurus lets you focus on your docs, and we&apos;ll do the chores. Go
-        ahead and move your docs into the <code>docs</code> directory.
+        <div>
+          Aquí recopilamos las presentaciones que usamos en semestres anteriores, desde básicos a intermedios, también encontrarás tutoriales y artículos útiles. ¡Mejora tus habilidades y no olvides pasarte por nuestro Blog!
+        </div>
+        <Link
+          className="button button--primary mt-3"
+          to="docs/intro">
+          Ver documentación
+          <i className="bi bi-book ml-3"></i>
+        </Link>
       </>
     ),
   },
   {
-    title: 'Powered by React',
-    Svg: require('@site/static/img/undraw_docusaurus_react.svg').default,
+    title: '¡Asiste al Club!',
+    Svg: require('@site/static/img/landing-page/attend-class.svg').default,
+    backgroundSvg: require('@site/static/img/landing-page/blob-2.svg').default,
     description: (
       <>
-        Extend or customize your website layout by reusing React. Docusaurus can
-        be extended while reusing the same header and footer.
+        <div>
+          Siguiendo un temario definido al inicio del semestre, damos cátedra de diferentes temas clave para entrar a la programación competitiva. Revisa los horarios y aulas en el semestre en curso, ¡te esperamos!
+        </div>
+        <Link
+          className="button button--primary mt-3"
+          to="blog/horarios">
+          Ver horarios
+          <i className="bi bi-calendar ml-3"></i>
+        </Link>
       </>
     ),
   },
 ];
 
-function Feature({title, Svg, description}: FeatureItem) {
+function Feature({title, Svg, backgroundSvg, description}: FeatureItem) {
   return (
     <div className={clsx('col col--4')}>
       <div className="text--center">
